83052f1fff6956649e8446c8d1599fc344269df5
channie <aihoshiichan@gmail.com>
2026-07-30 13:00:11 +0100

    commitTITTIESSSSS- i got distracted. ahem. i'm stra-

--- commands/roles.js
+++ commands/roles.js
@@ -1,1 +1,11 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("roles")
+        .setDescription("🌸 Opens Shiobun's adorable Role Garden!"),
+
+    async execute(interaction) {
+
+    }
+};
