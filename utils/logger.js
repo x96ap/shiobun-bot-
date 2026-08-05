29cba4083abb5cf81133ed1aafeeee54b7c11a7b
channie <aihoshiichan@gmail.com>
2026-08-05 12:51:05 +0100

    Add logging functions with default bot name

--- utils/logger.js
+++ utils/logger.js
@@ -1,1 +1,21 @@
+const defaults = require("../config/defaults");
 
+module.exports = {
+
+    info(message) {
+        console.log(`🌸 ${defaults.botName} │ ${message}`);
+    },
+
+    success(message) {
+        console.log(`✨ ${defaults.botName} │ ${message}`);
+    },
+
+    warning(message) {
+        console.log(`🍓 ${defaults.botName} │ ${message}`);
+    },
+
+    error(message) {
+        console.log(`🥺 ${defaults.botName} │ ${message}`);
+    }
+
+};
