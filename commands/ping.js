df45d8f336a012d8f81cfa621cfc0cd407fc9c59
channie <aihoshiichan@gmail.com>
2026-08-16 23:19:26 +0100

    Add ping command to check bot status

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("check if sorimi is awake!"),
+
+    async execute(interaction) {
+        await interaction.reply("૮꒰ ˶• ༝ •˶꒱ა ♡ i'm awake!!");
+    }
+};
